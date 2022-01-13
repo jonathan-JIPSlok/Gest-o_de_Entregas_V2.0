4845eaa6523174285cefaf31cb45da9b5a86d222
JIPSlok <jonathanpoli17@gmail.com>
2022-01-13 09:27:09 -0300

    arrumando tabelas e verificações de dados

--- Modules/database.py
+++ Modules/database.py
@@ -4,17 +4,56 @@ from random import randint
 
 class user:
     def __init__(self):
-        self.LOCAL = "Dados" + sep + "Users.db"
+        self.LOCAL = "Dados" + sep + "Users.db" #, Local do arquivo
+        self.connect() #Cria a conexão
+        self.create_tables() #Cria as tabelas se elas não existirem
         
     def connect(self):
+        """Cria uma conexão com o banco de dados e cria o cursor"""
         self.CONECTOR = sqlite3.connect(self.LOCAL)
         self.CURSOR = self.CONECTOR.cursor()
-        self.create_tables()
         
     def create_tables(self):
-        self.CURSOR.execute("CREATE TABLE IF NOT EXISTS Usuarios(ID interger primary key, Nome text, Tipo text, Email text, Numero text, RG text, CPF text, CEP text)")
+        """Cria as tabelas caso elas não existão"""
+        self.CURSOR.execute("""CREATE TABLE IF NOT EXISTS Usuarios(ID INTERGER PRIMARY KEY, 
+        Nome TEXT NOT NULL UNIQUE, 
+        Senha TEXT NOT NULL, 
+        Tipo TEXT NOT NULL, 
+        Email TEXT NOT NULL, 
+        Celular TEXT NOT NULL, 
+        RG TEXT NOT NULL, 
+        CPF TEXT NOT NULL, 
+        CEP TEXT NOT NULL,
+        Numero INTERGER NOT NULL,
+        Complemento TEXT)""")
         
     def cadastrar_user(self, data):
-        self.CURSOR.execute("INSERT INTO Usuarios(ID, Nome, Tipo, Email, Numero, RG, CPF, CEP) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (randint(100000000, 999999999), data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+        """Realiza o cadastro de um usuario
+        data -> deve conter uma lista com todos os dados do usuario"""
+        
+        #Realiza o cadastro
+        try:
+            self.CURSOR.execute("INSERT INTO Usuarios(ID, Nome, Senha, Tipo, Email, Celular, RG, CPF, CEP, Numero, Complemento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (randint(100000000, 999999999), data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]))
+        
+        except sqlite3.IntegrityError as erro:
+            print(str(erro) + "\nModulo: Database  \nFunção: cadastrar_user")
+            return False, "NOME DE USUÁRIO JÁ CADASTRADO OU DADOS FALTANDO"
+        
+        #salva o cadastro
+        self.CONECTOR.commit()
+        
+        #finaliza a conexão
         self.CONECTOR.close()
         
+        return True, "CADASTRO REALIZADO COM SUCESSO"
+        
+    def get_user(self, name):
+        """Pega um usuario
+        name -> deve conter o nome do usuario"""
+        resultado = self.CURSOR.execute("SELECT * FROM Usuarios  WHERE Nome = ?", (name, )).fetchall()
+        self.CONECTOR.close()
+        
+        if len(resultado) != 0:
+            return True, resultado[0]
+        else:
+            return False, "USUARIO NÃO ENCONTRADO"
--- Modules/users.py
+++ Modules/users.py
@@ -1,16 +1,75 @@
-import database
+from Modules import database, validator
+import hashlib
 
 class user:
-    def __init__(self, data, type, entregas):
-        self.ID = data[0]
-        self.Name = data[1]
-        self.TYPE = type
-        self.ENTREGAS = entregas
+    def __init__(self, user):
+        self.__ID = user[0]
+        self.__Name = user[1]
+        self.__TYPE = user[2]
         
 class new_user:
-    def __init__(self, name, email, number, rg, cpf, cep, type):
-        self.DATA = [name, type, email, number, rg, cpf, cep]
+    def __init__(self, name, senha, type, email, celular, rg, cpf, cep, numero, complemento = ""):
+        """Cadastramento de um novo usuario"""
+        #hashiando senha
+        self.senha = hashlib.sha256(senha).digest()
+        self.name =  name
+        self.email = email
+        self.celular = celular
+        self.rg = rg
+        self.cpf = cpf
+        self.cep = cep
+        self.numero = numero
+        self.complemento = complemento
+        self.type = type
     
-    def cadastar(self):
-        database.user().cadastrar_user(self.DATA)
+    def cadastrar(self):
+        """Efetua o cadastro"""
+        try:
+            if self.validacao == True:
+                data = [self.name, self.senha, self.type, self.email, self.celular, self.rg, self.cpf, self.cep, self.numero, self.complemento]
+                return database.user().cadastrar_user(data)
+            else:
+                return False, "DADOS INVALIDOS"
+        except AttributeError:
+            return False, "DADOS NÃO FORAM VALIDADOS"
+            
+    def validar(self):
+        """Valida os dados do Usuario"""
+        self.validacao = True
+        email = validator.email(self.email)
+        if email[0] == False:
+            self.validacao = False
+            return email
+        elif validator.cpf(self.cpf) == False:
+            self.validacao = False
+            return False, "CPF INVALIDO"
+        elif validator.cep(self.cep)[0] == False:
+            self.validacao = False
+            return validator.cep(self.cep)
         
+        else:
+            self.email = email
+            self.validacao = True
+            return True
+        
+        
+class get_user:
+    def __init__(self, Name, Senha):
+        """Obtem dados de um usuario"""
+        self.Senha = Senha
+        self.__USER = database.user().get_user(Name)
+        
+    def password_verify(self, password_db, password_user):
+        """Verifica se a senha esta correta"""
+        if hashlib.sha256(password_user).digest() == password_db:
+            return True
+            
+    @property
+    def USER(self):
+        """Retorna o usuario se a senha estiver correta se não retorna False"""
+        if self.__USER != False and self.password_verify(self.__USER[1][2], self.Senha):
+            user = self.__USER[1][0:2]
+            user += self.__USER[1][3:]
+            return user
+        else:
+            return False, "USUARIO OU SENHA INVALIDO"
